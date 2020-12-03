77e74f6618aef23708f884418c56fdf2619a1297
Artyom Lazarev <43204713+lartyom@users.noreply.github.com>
2020-12-03 17:52:50 +0300

    Update MainController.java

--- src/ru/imult/mult/pirate/MainController.java
+++ src/ru/imult/mult/pirate/MainController.java
@@ -116,7 +116,7 @@ public class MainController {
     	Alert alert = new Alert(AlertType.INFORMATION);
     	alert.setHeaderText("FreeMoolt v.1.2");
     	alert.setGraphic(new ImageView(this.getClass().getResource("icon.png").toString()));
-    	alert.setContentText("This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.");
+    	alert.setContentText("");
     	alert.show();
 
     }
